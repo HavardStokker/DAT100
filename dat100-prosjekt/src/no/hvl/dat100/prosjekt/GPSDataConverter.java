4c7121509b3b12288a716fd3bd35e7c20d1123f6
HavardStokker <42800438+HavardStokker@users.noreply.github.com>
2018-09-28 14:01:59 +0200

    Add files via upload

---
+++ dat100-prosjekt/src/no/hvl/dat100/prosjekt/GPSDataConverter.java
@@ -0,0 +1,113 @@
+package no.hvl.dat100.prosjekt;
+import static java.lang.Integer.*;
+import java.sql.Timestamp;
+import java.text.ParseException;
+//import org.joda.time.DateTime;
+//import org.joda.time.format.DateTimeFormat;
+//import org.joda.time.format.DateTimeFormatter;
+
+public class GPSDataConverter {
+
+	// arrays for GPS data in the original representation as array of strings
+	private String[] timesstr, latitudesstr, longitudesstr, elevationsstr;
+	private int arrayLength;
+	
+	// arrays for GPS data that we would like to transform to
+	// public to simplify later code and since get/set methods have not yet been covered in the course
+	public static int[] times;
+	public double[] latitudes, longitudes, elevations;
+	
+	public GPSDataConverter(GPSData gpsdata) {
+		
+		// initialize the arrays for the original representation
+		timesstr = gpsdata.getTimes();
+		latitudesstr = gpsdata.getLattitudes();
+		longitudesstr = gpsdata.getLongitudes();
+		elevationsstr = gpsdata.getElevations();
+		
+		arrayLength = timesstr.length; // denne representerer antall rader i arrayene over.
+	}
+
+	// konverter tidsinformasjon i gps data punkt til antall sekunder fra midnatt
+	// dvs. ignorer information om dato og omregn tidspunkt til sekunder
+	// Eksempel - tidsinformasjon (som String): 2017-08-13T08:52:26.000Z
+    // skal omregnes til sekunder (som int): 8 * 60 * 60 + 52 * 60 + 26 
+	
+	public static int TIME_STARTINDEX = 11; // startindex for tidspunkt
+
+	public static int toSeconds(String timestr) {
+		
+		int sekunderSidenMidnatt = 0;
+		int t, min, sekunder;
+		t = Integer.parseInt(timestr.substring(TIME_STARTINDEX, 13));
+		min = Integer.parseInt(timestr.substring(14, 16));
+		sekunder = Integer.parseInt(timestr.substring(17,19));
+		sekunderSidenMidnatt = t*3600 + min*60 + sekunder;
+		
+		return sekunderSidenMidnatt;
+	}
+	
+		// TODO
+		// OPPGAVE - START
+		
+	
+			
+			
+		
+	
+		// OPPGAVE - SLUTT
+
+	// konverter representation av data fra strenger (String) til tall
+	public void convert() {
+
+		// antall GPS datapunkter
+		int n = arrayLength;
+
+		// tabeller for konvertert gps data
+		times = new int[n];			// ny tabell for tidsinformasjon i sekunder (int)
+		latitudes = new double[n];  // ny tabell for breddegrad (double)
+		longitudes = new double[n]; // ny tabell for lengdegrad (double)
+		elevations = new double[n]; // ny tabell for høyde (double)
+
+		// tabeller som skal konverteres fra er
+		// timesstr, latitudesstr, longitudesstr, elevationsstr erklært først i klassen
+		
+		// TODO
+		// OPPGAVE - START
+		for (int i=0; i<n; i++) {
+			times[i] = toSeconds(timesstr[i]);
+			latitudes[i] = Double.parseDouble(latitudesstr[i]);
+			longitudes[i] = Double.parseDouble(longitudesstr[i]);
+			elevations[i] = Double.parseDouble(elevationsstr[i]);
+			
+		}
+		
+		// Hint:
+		// iterer igjennom alle gps punkter (hint: bruk en for-løkke)
+		// konverter hver inngang gps datapunkt 
+		// - tidsinformasjon til sekunder (int) (hint: bruk toSeconds)
+		// - breddegrad til double (hint: bruk Double.parseDouble)
+		// - lengdegrad til double 
+		// - høyde til double
+        // sett konvertert data inn på rett plass i den rette tabellen
+		
+		// END
+		// OPPGAVE - SLUTT ;
+	}
+	
+	// skriv ut konvertert GPS data op formatet:
+	// sekunder (breddegrad,lengdegrad) høyde
+	public void print() {
+		
+		System.out.println();
+		System.out.println("Konvertert GPS Data");
+		// TODO
+		// OPPGAVE - START
+		
+		int i = 0;
+		for(i=0; i<arrayLength; i++ ) {
+			System.out.println(times[i] + " (" + latitudes[i] + "," + longitudes[i] + ") " + elevations[i]);
+		}
+		// OPPGAVE - SLUTT
+	}
+}
---
+++ dat100-prosjekt/src/no/hvl/dat100/prosjekt/GPSUtils.java
@@ -0,0 +1,114 @@
+package no.hvl.dat100.prosjekt;
+import static java.lang.Math.*;
+import java.text.SimpleDateFormat ;
+import java.util.Date ;
+import java.util.TimeZone;
+
+
+public class GPSUtils {
+
+	public GPSUtils() {
+	
+	}
+	
+	// konverter sekunder til string på formen hh:mm:ss
+	public static String printTime(int secs) {
+		
+		String timestr = "";
+		SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
+		// TODO
+		// OPPGAVE - START
+		Date d = new Date(secs * 1000L); // HH for 0-23
+		df.setTimeZone(TimeZone.getTimeZone("GMT"));
+		timestr = df.format(d);
+		// OPPGAVE - SLUTT
+		
+		return timestr;
+	}
+	
+	// beregn maximum av en tabell av doubles med minnst et element
+	public static double findMax(double[] da) {
+
+		double max = da[0];
+
+		for (double d : da) {
+			if (d > max) {
+				max = d;
+			}
+		}
+
+		return max;
+	}
+
+	// beregn minimum av en tabell av doubles med minnst et element
+	public static double findMin(double[] da) {
+
+		// fjern = "0.0" når metoden implementeres for ikke få forkert minimum
+		double min = da[0];
+		
+		// TODO
+		// OPPGAVE - START
+		for(double d : da) {
+			if(d < min) {
+				min = d;
+			}
+		}
+		// OPPGAVE - SLUT
+		return min;
+	}
+
+	
+	private static int R = 6371000; // jordens radius
+	
+	// Beregn avstand mellom to gps punkter ved bruk av Haversine formlen
+	public static double distance(double latitude1, double longitude1, double latitude2, double longitude2) {
+
+		double a,c,d; // fjern = 1.0
+		
+		// TODO:
+		// OPPGAVE - START
+		double phi1 = toRadians(latitude1);
+		double phi2 = toRadians(latitude2);
+		double dphi1 = toRadians(latitude2 - latitude1);
+		double dLamd1 = toRadians(longitude2 - longitude1);
+		
+		a = Math.pow((sin(dphi1/2)),2)+ cos(phi1)*cos(phi2)*Math.pow(sin(dLamd1/2),2);
+		c = 2*atan2(sqrt(a), sqrt((1-a)));
+		d = R*c;
+		// OPPGAVE - SLUTT
+
+		return d;
+	}
+	
+	// beregn gjennomsnits hastighet i km/t mellom to gps punkter
+	public static double speed(int secs, double latitude1, double longitude1, double latitude2, double longitude2) {
+
+		double speed;
+
+		// TODO:
+		// OPPGAVE - START
+	   double d = distance(latitude1, longitude1, latitude2, longitude2);
+	   speed = (d/secs)*3.6;
+		// OPPGAVE - SLUTT
+	   return speed;
+
+	
+	}
+	
+	private static int TEXTWIDTH = 10;
+	// konverter double til string med 2 decimaler og streng lengde 10
+	// eks. 1.346 konverteres til "      1.35" og enhet til slutt
+	// Hint: se på String.format metoden
+	
+	public static String printDouble(double d) {
+		String str = "";
+		
+		
+		// TODO
+		// OPPGAVE - START
+		String numberAsString = String.format ("%10.2f", d);
+		str = numberAsString;
+		// OPPGAVE - SLUTT
+		return str;
+	}
+}
